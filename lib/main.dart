5cf992d6b3da21c6ab28306637d5da7f9eb8d9c3
SanSpeen <ktns0930@gmail.com>
2024-03-22 22:23:54 -0500

    Added some functionalities for addind items to the list

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,73 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(TodoApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class TodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final List<String> _todos = [];
+
+  void _addTodo() {
+    setState(() {
+      String newTodo = _controller.text;
+      if (newTodo.isNotEmpty) {
+        _todos.add(newTodo);
+        _controller.clear();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              labelText: 'Enter your task',
+            ),
+            onSubmitted: (_) => _addTodo(),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_todos[index]),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTodo,
+        tooltip: 'Add Todo',
+        child: Icon(Icons.add),
       ),
     );
   }
